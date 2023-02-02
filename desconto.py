726d71ed276be3761f8a2e18203bf25c583dfec8
Elivandro Santos <elivandrocsantos@gmail.com>
2023-02-02 17:27:53 -0300

    Create desconto.py

---
+++ desconto.py
@@ -0,0 +1,8 @@
+m = float(input('Preço: '))
+p = float(input('Desconto em % : '))
+
+desconto = m * p / 100
+novo = m - desconto
+
+print(f'Desconto: R$ {desconto:.2f}')
+print(f'Preço a pagar: {novo:.2f}')
