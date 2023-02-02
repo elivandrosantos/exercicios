bc21c6ed35366dd7ec7b98dceddaf3e51f5e490c
Elivandro Santos <elivandrocsantos@gmail.com>
2023-02-02 10:15:52 -0300

    Create calc_area_square.py

---
+++ calc_area_square.py
@@ -0,0 +1,13 @@
+def areaQuadrado():
+    # a = lateral ** 2
+    # return a * 2
+
+    # lateral = int(input('Insira o valor da lateral: '))
+    # print('O valor do dobro da área do quadrado é de', (areaQuadrado(lateral))
+
+    lateral = float(input('Insira o valor da lateral: '))
+    a = lateral ** 2
+    dobro = a * 2
+    print('O valor da area em dobro é', dobro)
+
+areaQuadrado()
